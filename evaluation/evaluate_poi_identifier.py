1d815f306167b0e1bba8ba3ac89596cf0b756944
Unknown <davidcblum@gmail.com>
2017-02-24 15:46:53 -0800

    sort_keys test

--- evaluation/evaluate_poi_identifier.py
+++ evaluation/evaluate_poi_identifier.py
@@ -29,33 +29,97 @@ all_features = ["poi", "salary", "to_messages", "deferral_payments",
 current_max_features_list = ["poi", "exercised_stock_options",
 "deferred_income", "total_stock_value", "long_term_incentive", "expenses"]
 
-features_list = ["poi", "exercised_stock_options", "deferred_income",
-"total_stock_value", "long_term_incentive", "expenses"]
-
-data = featureFormat(data_dict, features_list)
+features_list = ["poi", "exercised_stock_options",
+"deferred_income", "total_stock_value", "long_term_incentive", "expenses"]
 
 
-# Standardize data
-from sklearn.preprocessing import scale
-from sklearn.preprocessing import StandardScaler
+# Remove Outliers
+del data_dict['TOTAL']
+del data_dict['THE TRAVEL AGENCY IN THE PARK']
+
+''' # Find Poi with missing features
+# Create dictionary of features with list of poi with associated missing feature
+from pprint import pprint
+missing_dict = dict()
+for feature in features_list:
+    missing_persons = []
+    for person in data_dict:
+        if data_dict[person]['poi'] == True:
+            if data_dict[person][feature] == "NaN":
+                missing_persons.append(person)
+    if len(missing_persons) > 0:
+        missing_dict[feature] = missing_persons
+
+pprint(missing_dict)
+
+# Finds poi with a SINGLE NaN in feature list
+missing_list = list()
+for feature in missing_dict:
+    missing_list.append(missing_dict[feature])
+missing_list = [item for sublist in missing_list for item in sublist]
+from collections import Counter
+missing_list = Counter(missing_list)
+
+# Finds poi with NaN in ALL feature list
+WARNING_LIST = list()
+for person in missing_list:
+    if missing_list[person] >= len(missing_dict):
+        WARNING_LIST.append(person)
+print "POI missing ALL features: ", WARNING_LIST
+'''
+
+
+''' # Normalize Data
+from sklearn.preprocessing import normalize
+import numpy as np
+
+# Create list of arrays to normalize, then normalize
+normalized_features_list = []
+normalized_featuresbyname_dict = {}
+# features_list[1:] to ignore if poi
+for feature in features_list[1:]:
+    feature_need_scale = []
+    for person in data_dict:
+        value = data_dict[person][feature]
+        if value != 'NaN':
+            feature_need_scale.append(value)
+
+    normalized_features_list.append(normalize([feature_need_scale]))
+
+print normalized_features_list
+'''
+
+'''
+# Place normalized values back into data_dict
+for feature in features_list[1:]:
+        for person in range(len(data_dict)):
+            number = data_dict[person][feature]
+            if number != 'Nan':
+                data_dict[person][feature] =
+'''
+
+data = featureFormat(data_dict, features_list, sort_keys = True)
+
+print data
+''' # Normalize Data after featureFormat
+# Prepare normalization of data
 from sklearn.preprocessing import normalize
 
-# Create empty features list of lists
+# Create empty features list of lists ignoring poi
 data_need_scale = [[] for i in range(len(features_list)-1)]
 for person in data:
     for i in range(len(features_list)-1):
         data_need_scale[i].append(person[i+1])
 
-# Scale each feature in the list
+# Scale each feature in the list (poi is not in list)
 data_scaled = normalize(data_need_scale)
 
-# Placed scaled features back into data
+# Placed scaled features back into data (don't write into poi)
 for person in range(len(data)):
     for feature in range(len(features_list)-1):
         data[person][feature+1] = data_scaled[feature][person]
-
-####
-
+'''
+# Create labels and features
 labels, features = targetFeatureSplit(data)
 
 ### your code goes here
@@ -65,9 +129,8 @@ from sklearn.metrics import classification_report
 from sklearn.metrics import confusion_matrix
 from sklearn.metrics import precision_score
 from sklearn.metrics import recall_score
-
-
 from sklearn.metrics import accuracy_score
+
 
 features_train, features_test, labels_train, labels_test = train_test_split(
 features, labels, test_size = .3, random_state = 42)
@@ -78,11 +141,9 @@ def pound_line():
 def try_classifier(name, clf_choice):
     pound_line()
     print " " * 20 + name + "\n"
-
     print "Features: ", features_list
 
     clf = clf_choice
-
     t0 = time()
     clf.fit(features_train, labels_train)
     print "Training Time:", round(time()-t0, 3), 's'
@@ -90,7 +151,6 @@ def try_classifier(name, clf_choice):
     t0 = time()
     labels_pred = clf.predict(features_test)
     print "Predict Time:", round(time()-t0, 3), 's'
-
     print "Accuracy Score:", accuracy_score(labels_pred, labels_test)
     print "# of Features:", len(features[0])
 
@@ -102,14 +162,12 @@ def try_classifier(name, clf_choice):
     print "Precision Score: ", precision_score(labels_test, labels_pred)
     print "Recall Score: ", recall_score(labels_test, labels_pred)
 
-
-
-
-##### Decion Tree #####
+##### Decision Tree #####
 # Seems to work best with specfic selected features
 from sklearn import tree
 try_classifier("Decision Tree", tree.DecisionTreeClassifier())
 
+'''
 ##### Random Forest #####
 # Does okay...
 from sklearn.ensemble import RandomForestClassifier
@@ -130,3 +188,4 @@ try_classifier("SVM poly", SVC(C=20, kernel='poly'))
 # Naive Bayes never predicts true positive, but can predict true negative.
 from sklearn.naive_bayes import GaussianNB
 try_classifier("Naive Bayes", GaussianNB())
+'''
--- final_project/poi_id.py
+++ final_project/poi_id.py
@@ -10,20 +10,46 @@ from tester import dump_classifier_and_data
 ### Task 1: Select what features you'll use.
 ### features_list is a list of strings, each of which is a feature name.
 ### The first feature must be "poi".
-features_list = ['poi','salary'] # You will need to use more features
+features_list = ["poi", "exercised_stock_options", "deferred_income",
+ "total_stock_value", "long_term_incentive", "expenses"]
+ # You will need to use more features
 
 ### Load the dictionary containing the dataset
 with open("final_project_dataset.pkl", "r") as data_file:
     data_dict = pickle.load(data_file)
 
 ### Task 2: Remove outliers
+del data_dict['TOTAL']
+del data_dict['THE TRAVEL AGENCY IN THE PARK']
+
 ### Task 3: Create new feature(s)
+
+
 ### Store to my_dataset for easy export below.
 my_dataset = data_dict
+
+from pprint import pprint
+print(my_dataset)
 
 ### Extract features and labels from dataset for local testing
 data = featureFormat(my_dataset, features_list, sort_keys = True)
 labels, features = targetFeatureSplit(data)
+
+'''Changed sort_keys to False. Somehow getting very poor clf from True'''
+
+
+### Task 5: Tune your classifier to achieve better than .3 precision and recall
+### using our testing script. Check the tester.py script in the final project
+### folder for details on the evaluation method, especially the test_classifier
+### function. Because of the small size of the dataset, the script uses
+### stratified shuffle split cross validation. For more info:
+### http://scikit-learn.org/stable/modules/generated/sklearn.cross_validation.StratifiedShuffleSplit.html
+
+# Example starting point. Try investigating other evaluation techniques!
+from sklearn.cross_validation import train_test_split
+features_train, features_test, labels_train, labels_test = train_test_split(
+    features, labels, test_size=0.3, random_state=42)
+
 
 ### Task 4: Try a varity of classifiers
 ### Please name your classifier clf for easy export below.
@@ -31,21 +57,45 @@ labels, features = targetFeatureSplit(data)
 ### you'll need to use Pipelines. For more info:
 ### http://scikit-learn.org/stable/modules/pipeline.html
 
-# Provided to give you a starting point. Try a variety of classifiers.
-from sklearn.naive_bayes import GaussianNB
-clf = GaussianNB()
+from time import time
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import precision_score
+from sklearn.metrics import recall_score
+from sklearn.metrics import accuracy_score
 
-### Task 5: Tune your classifier to achieve better than .3 precision and recall 
-### using our testing script. Check the tester.py script in the final project
-### folder for details on the evaluation method, especially the test_classifier
-### function. Because of the small size of the dataset, the script uses
-### stratified shuffle split cross validation. For more info: 
-### http://scikit-learn.org/stable/modules/generated/sklearn.cross_validation.StratifiedShuffleSplit.html
+def pound_line():
+    print "\n" + "#" * 60
 
-# Example starting point. Try investigating other evaluation techniques!
-from sklearn.cross_validation import train_test_split
-features_train, features_test, labels_train, labels_test = \
-    train_test_split(features, labels, test_size=0.3, random_state=42)
+def try_classifier(name, clf):
+    pound_line()
+    print " " * 20 + name + "\n"
+    print "Features: ", features_list
+
+    t0 = time()
+    clf.fit(features_train, labels_train)
+    print "Training Time:", round(time()-t0, 3), 's'
+
+    t0 = time()
+    labels_pred = clf.predict(features_test)
+    print "Predict Time:", round(time()-t0, 3), 's'
+    print "Accuracy Score:", accuracy_score(labels_pred, labels_test)
+    print "# of Features:", len(features[0])
+
+    print
+    print classification_report(labels_test, labels_pred)
+    print confusion_matrix(labels_test, labels_pred)
+    print
+
+    print "Precision Score: ", precision_score(labels_test, labels_pred)
+    print "Recall Score: ", recall_score(labels_test, labels_pred)
+
+##### Decision Tree #####
+# Seems to work best with specfic selected features
+from sklearn import tree
+clf = tree.DecisionTreeClassifier()
+try_classifier("Decision Tree", clf)
 
 ### Task 6: Dump your classifier, dataset, and features_list so anyone can
 ### check your results. You do not need to change anything below, but make sure
